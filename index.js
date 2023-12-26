5bd34407f6506ccfc75daf00dcb3dbcdabe15fe3
aristrisnawan <aristrisnawan35@gmail.com>
2023-12-26 21:56:56 +0700

    connect to database

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes/routes')
+const mongoString = process.env.DATABASE_URL
+const app = express()
+
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on('error',(error) => {
+    console.log('error anjing');
+})
+
+database.once('connected', () => {
+    console.log('Database connected');
+})
+
+
+app.use('/api',routes)
+
+app.use(express.json())
+app.listen(3000, () => {
+    console.log(`Server started at ${3000}`);
+})
+
+
